9ddea9d035859f56357567400454a1c33f4b35e2
AndrisGrindulis <40773420+AndrisGrindulis@users.noreply.github.com>
2023-04-24 08:17:23 +0300

    last day
    last date ys progress

---
+++ src/test/java/apiApril/constants/ProjectConstants.java
@@ -0,0 +1,8 @@
+package apiApril.constants;
+
+public class ProjectConstants {
+    public static final String API_KEY = "c74be224f3b5b49b0fcebd91f9a60081";
+    public static final String API_TOKEN = "ATTA4607b8b65b78e3675b718bcd52b9fb83679bcd7fed6977b56d4de8cff2e104089703C237";
+    public static final String BOARD_ID = "6444e81cba4cb30b9515db2b";
+    public static final String BOARD_NAME = "default";
+}
--- src/test/java/apiApril/stepdefinitions/TrelloSteps.java
+++ src/test/java/apiApril/stepdefinitions/TrelloSteps.java
@@ -1,29 +1,59 @@
 package apiApril.stepdefinitions;
 
+import apiApril.domain.Board;
+import apiApril.domain.List;
+import apiApril.helpers.TestCaseContext;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import junit.framework.TestCase;
+import org.assertj.core.api.Assertions;
+
+//import static apiApril.clients.TrelloClient.*;
+import static apiApril.clients.TrelloClient.getBoardInfo;
+import static apiApril.clients.TrelloClient.updateBoardInfo;
+import static apiApril.clients.TrelloClient.createList;
+import static apiApril.constants.ProjectConstants.BOARD_ID;
+import static apiApril.constants.ProjectConstants.BOARD_NAME;
 
 public class TrelloSteps {
 
     @Given("The board exists and contains the correct information")
     public void getBoardDataAndCheckInfo(){
-        System.out.println("one");
+        Response response = getBoardInfo(BOARD_ID);
+        Board board = response.as(Board.class);
+        Assertions.assertThat(board.getId())
+                .as("We assert that the board ID is correct")
+                .isEqualTo(BOARD_ID);
+        Assertions.assertThat(board.getName())
+                .as("we assert that the board Name is correct")
+                .isEqualTo(BOARD_NAME);
     }
-
     @When("I change the board title to {string}")
     public void iChangeTheBoardTitleTo(String title) {
-        System.out.println("one");
+        Response response = updateBoardInfo(title, BOARD_ID);
+        Board board = response.as(Board.class);
+        TestCaseContext.setBoard(board);
     }
 
     @And("I check that the board name was updated to {string}")
-    public void iCheckThatTheBoardNameWasUpdatedTo(String title) {
-        System.out.println("one");
+    public void iCheckThatTheBoardNameWasUpdatedTo(String title){
+        Assertions.assertThat(TestCaseContext.getBoard().getName())
+                .as("We check that the board name is updated to " + title)
+                .isEqualTo(title);
     }
 
     @Then("I add a list with a name {string} to the board")
     public void iAddAListWithANameToTheBoard(String listName) {
-        System.out.println("one");
+        Response response = createList(listName, TestCaseContext.getBoard().getId());
+        List list = response.as(List.class);
+        TestCaseContext.setList(list);
+        Assertions.assertThat(list.getName())
+                .as("We chech that list was created correcly")
+                .isEqualTo(listName);
+
+        TestCaseContext.getScenario().log(list.getId());
     }
 }
